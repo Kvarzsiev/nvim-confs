81ad88d4cb3b6e9c1494ed7f1a864b8a3e96af66
Kvarzsiev <99659392+Kvarzsiev@users.noreply.github.com>
2024-01-10 16:46:50 -0300

    meow

---
+++ lua/config/lsp-config.lua
@@ -0,0 +1,32 @@
+-- Setup language servers.
+local lspconfig = require("lspconfig")
+
+local on_attach = function(client)
+  require("completion").on_attach(client)
+end
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  settings = {
+    ["rust-analyzer"] = {
+      imports = {
+        granularity = {
+          group = "module",
+        },
+        prefix = "self",
+      },
+      rustfmt = {
+        overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
+      },
+      cargo = {
+        features = "all",
+        buildScripts = {
+          enable = true,
+        },
+      },
+      procMacro = {
+        enable = true,
+      },
+    },
+  },
+})
