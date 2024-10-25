1ba3c368f4e3d3c20e51fbb324e82156bfc33317
Enzo <99659392+Kvarzsiev@users.noreply.github.com>
2024-10-25 11:19:14 -0300

    correcting flutter tools call

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -1,14 +1,32 @@
-require("flutter-tools").setup({
-  fvm = false,
-  flutter_lookup_cmd = "asdf where flutter",
-  lsp = {
-    color = { -- show the derived colours for dart variables
-      enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-      background = false, -- highlight the background
-      background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-      foreground = false, -- highlight the foreground
-      virtual_text = true, -- show the highlight using virtual text
-      virtual_text_str = "■", -- the virtual text character to highlight
+-- Setup language servers.
+local lspconfig = require("lspconfig")
+
+local on_attach = function(client)
+  -- require("completion").on_attach(client)
+end
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  settings = {
+    ["rust-analyzer"] = {
+      imports = {
+        granularity = {
+          group = "module",
+        },
+        prefix = "self",
+      },
+      rustfmt = {
+        overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
+      },
+      cargo = {
+        features = "all",
+        buildScripts = {
+          enable = true,
+        },
+      },
+      procMacro = {
+        enable = true,
+      },
     },
   },
-}) -- use defaults
+})
